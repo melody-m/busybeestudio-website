350f6af1a0115fd9e233a5d0765a2911b72947e3
Melody <mahoneymelody@gmail.com>
2019-02-14 12:49:18 +0000

    classes basic display slider

--- dist/js/index.js
+++ dist/js/index.js
@@ -7,6 +7,13 @@ const classesSlider = document.querySelectorAll('.classes__class');
 
 const classGallery = document.querySelector('.classes__gallery');
 
+const slideSideR = document.querySelector('.slideSideR');
+const slideSideL1 = document.querySelector('.slideSideL--1');
+const slideSideL2 = document.querySelector('.slideSideL--2');
+const slideSideL3 = document.querySelector('.slideSideL--3');
+const slideSideL4 = document.querySelector('.slideSideL--4');
+const slideSideL5 = document.querySelector('.slideSideL--5');
+
 const classesSliderArr = Array.from(classesSlider);
 
 
@@ -14,33 +21,51 @@ function slideExtend() {
     classesSliderArr.forEach((cur) => {
 
         cur.addEventListener('click', (e) => {
+            console.log(e.target);
 
-            if(e.target.classList.contains('classes__class--1')){
-                classGallery.style.backgroundImage = 'url("../img/pottery2.jpg")';
-            } else if(e.target.classList.contains('classes__class--1')){
-                classGallery.style.backgroundImage = 'url("../img/drawing.jpg")';
+            if (e.target.classList.contains('classes__class--1')){
+                classGallery.classList.add('classes__class--1');
             } else if(e.target.classList.contains('classes__class--2')){
-                classGallery.style.backgroundImage = 'url("../img/drawing.jpg")';
+                classGallery.classList.add('classes__class--2');
             } else if(e.target.classList.contains('classes__class--3')){
-                classGallery.style.backgroundImage = 'url("../img/sewing.jpg")';
+                classGallery.classList.add('classes__class--3');
             } else if(e.target.classList.contains('classes__class--4')){
-                classGallery.style.backgroundImage = 'url("../img/flower.jpg")';
+                classGallery.classList.add('classes__class--4');
             } else if(e.target.classList.contains('classes__class--5')){
-                classGallery.style.backgroundImage = 'url("../img/crea.jpg")';
+                classGallery.classList.add('classes__class--5');
             }
 
-
             classesSliderArr.forEach((cur) => {
-                if(cur != e.target){
-                    cur.classList.add('none');             
-                }
+                cur.classList.add('none');
+                // if(cur != e.target){
+                //     cur.classList.add('none');             
+                // }
                 
                 cur.addEventListener('transitionend', () => {
                     if (cur.classList.contains('none')){
                         cur.style.display = 'none';
                     }
-                    e.target.style.backgroundImage = 'url("")';
-                    e.target.classList.replace('width','extend');
+
+                    if (e.target.classList.contains('classes__class--1')){
+                        slideSideL1.style.transform = 'translateX(0%)';
+
+                    } else if(e.target.classList.contains('classes__class--2')){
+                        slideSideL2.style.transform = 'translateX(0%)';
+
+                    } else if(e.target.classList.contains('classes__class--3')){
+                        slideSideL3.style.transform = 'translateX(0%)';
+
+                    } else if(e.target.classList.contains('classes__class--4')){
+                        slideSideL4.style.transform = 'translateX(0%)';
+
+                    } else if(e.target.classList.contains('classes__class--5')){
+                        slideSideL5.style.transform = 'translateX(0%)';
+                    }
+
+                    slideSideR.style.transform = 'translateX(0%)';
+                    
+                    
+
                 })
             })            
         })     
