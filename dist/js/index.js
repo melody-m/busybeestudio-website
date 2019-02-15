770b53c74a98da28daac2c3cbed13fb25358125b
Melody <mahoneymelody@gmail.com>
2019-02-15 15:29:04 +0000

    gallery full screen next previous img working

--- dist/js/index.js
+++ dist/js/index.js
@@ -18,6 +18,10 @@ const closeBtnArr = Array.from(closeBtn);
 
 let bg;
 
+
+const photoGallery = document.querySelectorAll('.gallery__photo');
+const photoGalleryArr = Array.from(photoGallery);
+
 function slideExtend() {
     classesBtnArr.forEach((cur) => {
 
@@ -86,10 +90,121 @@ function closeClass() {
     })
 }
 
+function getID(e){
+    const fullID = e.target.id;
+    const splitID = fullID.split('-');
+    const ID = splitID[1];
+
+    return ID;
+}
+
+/////GALLERY//////////
+
+
+const gallery = {
+    fullScreen : document.querySelector('.gallery__fullScreen'),
+    fullImg : document.querySelector('.gallery__fullImg'),
+    closeBtn : document.querySelector('.gallery__close'),
+    allImg : document.querySelectorAll('.gallery__photo') ,
+    imgIdArr :[],
+    arrL : document.querySelector('.gallery__arrow--L'),
+    arrR : document.querySelector('.gallery__arrow--R'),
+}
+
+
+function getImgID(){
+    const fullImg = document.querySelector('.gallery__img');
+    const fullImgID = fullImg.id; 
+    const splitID = fullImgID.split('-');
+    const imgID = splitID[1];
+
+    return imgID;
+}
+
+
+function addFigure(id){
+
+    const newHtml = `<figure class="gallery__figure">
+            <img src="img/gallery-${id}.jpg"
+                 alt="Our studio lounge"
+                 class="gallery__img"
+                 id="img-${id}">
+            <figcaption class="gallery__caption">Our studio lounge</figcaption>
+            </figure>`
+
+    gallery.fullImg.insertAdjacentHTML('beforeend', newHtml);   
+
+}
+
+function removeFigure(){
+    const imgFigure = document.querySelector('.gallery__figure');            
+    imgFigure.parentNode.removeChild(imgFigure);    
+}
+
+function photoFullScreen() {
+
+    photoGalleryArr.forEach((cur) => {
+        cur.addEventListener('click', (e) => {         
+            
+            const photoID = getID(e);
+            gallery.fullScreen.style.visibility = 'visible';
+            addFigure(photoID);            
+        })
+    })
+}
+
+
+function closePhoto(){
+    gallery.closeBtn.addEventListener('click', () => {        
+        gallery.fullScreen.style.visibility = 'hidden';  
+        removeFigure();
+    })
+}
+
+function nextPhoto(){     
+
+    gallery.arrR.addEventListener('click',() => {      
+
+        const ID = getImgID();   
+        const nextID = parseInt(ID) +1;
+
+        if(ID < 10 ){
+            //Remove previous figure
+            removeFigure();
+            //Add next one
+            addFigure(nextID);    
+        }
+    })   
+}
+
+function previousPhoto(){
+
+    gallery.arrL.addEventListener('click',() => {
+        const ID = getImgID();   
+        const previousID = parseInt(ID) -1;
+
+        if(ID > 1){
+            //Remove previous figure
+            removeFigure();
+            //Add next one
+            addFigure(previousID);
+        }
+    })
+}
+//////
+
+//EVENT LISTENERS
+
+
+
 
 
 slideExtend();
 closeClass();
+photoFullScreen();
+closePhoto();
+nextPhoto();
+previousPhoto();
 
 
 
