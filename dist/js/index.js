9806ce12aed69b116cc8bcc7c6335c944de320f1
Melody <mahoneymelody@gmail.com>
2019-02-15 16:32:10 +0000

    rearrange js

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,67 +2,73 @@
 console.log('start');
 
 
-const classGallery = document.querySelector('.classes__gallery');
+const classesDOM = {
+    classGallery : document.querySelector('.classes__gallery'),
+    classesBtn : document.querySelectorAll('.classes__subtitle'),
+    classesSlider : document.querySelectorAll('.classes__class'),
+    slideSideR : document.querySelector('.classes__slideR'),
+    closeBtn : document.querySelectorAll('.classes__content--close'),
+}
 
-const classesBtn = document.querySelectorAll('.classes__subtitle');
-const classesBtnArr = Array.from(classesBtn);
+//const classGallery = document.querySelector('.classes__gallery');
 
-const classesSlider = document.querySelectorAll('.classes__class');
-const classesSliderArr = Array.from(classesSlider);
+//const classesBtn = document.querySelectorAll('.classes__subtitle');
+const classesBtnArr = Array.from(classesDOM.classesBtn);
 
-const slideSideR = document.querySelector('.classes__slideR');
+//const classesSlider = document.querySelectorAll('.classes__class');
+const classesSliderArr = Array.from(classesDOM.classesSlider);
+
+//const slideSideR = document.querySelector('.classes__slideR');
 
 
-const closeBtn = document.querySelectorAll('.classes__content--close');
-const closeBtnArr = Array.from(closeBtn);
+//const closeBtn = document.querySelectorAll('.classes__content--close');
+const closeBtnArr = Array.from(classesDOM.closeBtn);
 
 let bg;
+
+
 
 
 const photoGallery = document.querySelectorAll('.gallery__photo');
 const photoGalleryArr = Array.from(photoGallery);
 
-function slideExtend() {
-    classesBtnArr.forEach((cur) => {
 
-        cur.addEventListener('click', (e) => {            
-            
-            const fullID = e.target.id;
-            const splitID = fullID.split('-');
-            const ID = splitID[1];
-                      
-            
-            //Change BG image to the course selected
+function getID(e){
+    const fullID = e.target.id;
+    const splitID = fullID.split('-');
+    const ID = splitID[1];
 
-            const galClasses = listClasses();
-                        
-            if (galClasses[galClasses.length]){
+    return ID;
+}
 
-                //console.log(typeof (galClasses.length -1));
-                classGallery.classList.remove(`${galClasses[galClasses.length]}`);
-            };
 
-            //galClasses.length -1
-            
+////////////////////DECLARATION/////////////
 
-            classGallery.classList.add(`classBG-js-${ID}`);
-            
-            // Fades off the courses images
-            classesSliderArr.forEach((cur) => {
-                cur.classList.add('none');
-            })            
+function courseExtend(ID) {
+    
+    //List all the classes
+    const galClasses = listClasses();
+                            
+    if (galClasses.length > 1){ //if more than one class, remove the last one to change BG previously applied
+        classesDOM.classGallery.classList.remove(`${galClasses[galClasses.length -1]}`);
+    };             
 
-            //Sliders come in
-            document.querySelector(`.classes__slideL--${ID}`).style.transform = 'translateX(0%)';
-            document.querySelector(`.classes__slideR--${ID}`).style.transform = 'translateX(0%)';
-           
-        })     
+    //Add class to change BG
+    classesDOM.classGallery.classList.add(`classBG-js-${ID}`);
+    
+    // Fades off the courses images
+    classesSliderArr.forEach((cur) => {
+        cur.classList.add('none');
+    })            
 
-    })    
+    //Sliders come in
+    document.querySelector(`.classes__slideL--${ID}`).style.transform = 'translateX(0%)';
+    document.querySelector(`.classes__slideR--${ID}`).style.transform = 'translateX(0%)';   
 }
 
 function listClasses(){
-    let classList = classGallery.classList;
+    //Get an array with all the classes on the gallery container
+    let classList = classesDOM.classGallery.classList;
     let classListArr =  Array.from(classList);
     return classListArr;
 }
@@ -90,18 +96,12 @@ function closeClass() {
     })
 }
 
-function getID(e){
-    const fullID = e.target.id;
-    const splitID = fullID.split('-');
-    const ID = splitID[1];
-
-    return ID;
-}
-
-/////GALLERY//////////
 
 
-const gallery = {
+/***************************************************GALLERY*********************************************************/
+
+
+const galleryDOM = {
     fullScreen : document.querySelector('.gallery__fullScreen'),
     fullImg : document.querySelector('.gallery__fullImg'),
     closeBtn : document.querySelector('.gallery__close'),
@@ -110,7 +110,6 @@ const gallery = {
     arrL : document.querySelector('.gallery__arrow--L'),
     arrR : document.querySelector('.gallery__arrow--R'),
 }
-
 
 function getImgID(){
     const fullImg = document.querySelector('.gallery__img');
@@ -132,8 +131,7 @@ function addFigure(id){
             <figcaption class="gallery__caption">Our studio lounge</figcaption>
             </figure>`
 
-    gallery.fullImg.insertAdjacentHTML('beforeend', newHtml);   
-
+    galleryDOM.fullImg.insertAdjacentHTML('beforeend', newHtml)   
 }
 
 function removeFigure(){
@@ -141,70 +139,81 @@ function removeFigure(){
     imgFigure.parentNode.removeChild(imgFigure);    
 }
 
-function photoFullScreen() {
+function photoFullScreen(ID) {
 
-    photoGalleryArr.forEach((cur) => {
-        cur.addEventListener('click', (e) => {         
-            
-            const photoID = getID(e);
-            gallery.fullScreen.style.visibility = 'visible';
-            addFigure(photoID);            
-        })
-    })
+    galleryDOM.fullScreen.style.visibility = 'visible';
+    addFigure(ID);
 }
 
 
 function closePhoto(){
-    gallery.closeBtn.addEventListener('click', () => {        
-        gallery.fullScreen.style.visibility = 'hidden';  
-        removeFigure();
-    })
+    galleryDOM.fullScreen.style.visibility = 'hidden';  
+    removeFigure();
 }
 
-function nextPhoto(){     
+function nextPhoto(){    
 
-    gallery.arrR.addEventListener('click',() => {      
+    const ID = getImgID();   
+    const nextID = parseInt(ID) +1;
 
-        const ID = getImgID();   
-        const nextID = parseInt(ID) +1;
-
-        if(ID < 10 ){
-            //Remove previous figure
-            removeFigure();
-            //Add next one
-            addFigure(nextID);    
-        }
-    })   
+    if(ID < 10 ){
+        //Remove previous figure
+        removeFigure();
+        //Add next one
+        addFigure(nextID);    
+    }
 }
 
 function previousPhoto(){
 
-    gallery.arrL.addEventListener('click',() => {
-        const ID = getImgID();   
-        const previousID = parseInt(ID) -1;
-
-        if(ID > 1){
-            //Remove previous figure
-            removeFigure();
-            //Add next one
-            addFigure(previousID);
-        }
-    })
+    const ID = getImgID();   
+    const previousID = parseInt(ID) -1;
+    if(ID > 1){
+        //Remove previous figure
+        removeFigure();
+        //Add next one
+        addFigure(previousID);
+    }
 }
-//////
+
+
+
 
 //EVENT LISTENERS
 
+function setEventListeners(){
+
+    //Course listeners
+
+    cur.addEventListener('click', (e) => {           
+            
+        const ID = getID(e); 
+        courseExtend(ID);                                          
+    })   
+
+    //Gallery listeners
+
+    photoGalleryArr.forEach((cur) => {
+        cur.addEventListener('click', (e) => {        
+            
+            const photoID = getID(e);
+            photoFullScreen(photoID);                        
+        })
+    })
+
+    galleryDOM.closeBtn.addEventListener('click', closePhoto);
+
+    galleryDOM.arrR.addEventListener('click', nextPhoto);  
+    galleryDOM.arrL.addEventListener('click', previousPhoto);  
+}
+
+function init(){
+    setEventListeners();
+}
 
 
-
-
-slideExtend();
 closeClass();
-photoFullScreen();
-closePhoto();
-nextPhoto();
-previousPhoto();
+init();
 
 
 
